6eb435aa691b6000b4ff6180a6d6c3527d6b73bd
liaromano <lia.romano@post.runi.ac.il>
2024-12-09 10:53:55 +0200

    hw5t

--- MyString.java
+++ MyString.java
@@ -66,7 +66,7 @@ public class MyString {
      */
     public static String spacedString(String str) {
         //// Replace the following statement with your code
-        String n="";
+        String n=" ";
         for(int i=0;i<str.length()-1;i++)
         {
             n=n+str.charAt(i)+" ";
@@ -112,7 +112,7 @@ public class MyString {
        String s="";
        for(int i=0;i<str2.length();i++)
        {
-        if(str1.indexOf(str2.charAt(i))!=-1)
+        if(str1.indexOf(str2.charAt(i))==-1)
         {
             s=s+str2.charAt(i);
         }
